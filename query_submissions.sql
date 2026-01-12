a49fd8924f9119b2f96dfdb3c1861aa53dd30427
selenanguyen980 <selenanguyen980@gmail.com>
2026-01-12 12:55:07 -0800

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -13,3 +13,18 @@ Prompt: Find the average number of bathrooms and bedrooms for each city’s prop
 SELECT city, property_type, AVG(bathrooms) AS n_bathrooms_avg, AVG(bedrooms) AS n_bedrooms_avg
 FROM airbnb_search_details
 GROUP BY city, property_type;
+
+
+Prompt: Count the number of unique users per day who logged in from either a mobile device or web. Output the date and the corresponding number of users.
+
+WITH total_logs AS (
+SELECT user_id, log_date
+FROM mobile_logs
+UNION ALL
+SELECT user_id, log_date
+FROM web_logs
+)
+
+SELECT COUNT(DISTINCT user_id) AS n_users, log_date
+FROM total_logs
+GROUP BY log_date;
