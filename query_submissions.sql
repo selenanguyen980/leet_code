cd7fc5ad28d52ed5d0450a0a307205ab6b8972f2
selenanguyen980 <selenanguyen980@gmail.com>
2026-01-12 12:30:52 -0800

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -6,3 +6,10 @@ FROM customers AS c
 LEFT JOIN orders AS o
 ON c.id = o.cust_id
 ORDER BY c.first_name, o.order_details ASC;
+
+
+Prompt: Find the average number of bathrooms and bedrooms for each city’s property types. Output the result along with the city name and the property type.
+
+SELECT city, property_type, AVG(bathrooms) AS n_bathrooms_avg, AVG(bedrooms) AS n_bedrooms_avg
+FROM airbnb_search_details
+GROUP BY city, property_type;
