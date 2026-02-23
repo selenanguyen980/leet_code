94b28092292d1e5dd7463394c8489df06ca0fe03
selenanguyen980 <selenanguyen980@gmail.com>
2026-02-22 16:52:26 -0800

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -344,3 +344,7 @@ SELECT
 FROM sales_performance
 WHERE salesperson = 'Samantha'
    OR salesperson = 'Lisa';
+
+
+-- Prompt 31: Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
+-- Based on the above, find the average popularity of the Hack per office location. Output the location along with the average popularity.
