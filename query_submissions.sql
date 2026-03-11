fc0e638e342ef400227b2f5a67cadef523849f48
selenanguyen980 <selenanguyen980@gmail.com>
2026-03-11 11:26:06 -0700

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -428,3 +428,11 @@ SELECT *
 FROM lyft_rides
 WHERE weather = 'rainy'
     AND hour < 12;
+
+
+-- Prompt 40: Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
+Output all details related to retrieved records.
+SELECT *
+FROM lyft_drivers
+WHERE yearly_salary <= 30000
+    OR yearly_salary >= 70000;
