1910f339d3cdf2973651b41457c614dc1fe793c8
selenanguyen980 <selenanguyen980@gmail.com>
2026-03-17 11:55:19 -0700

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -436,3 +436,10 @@ SELECT *
 FROM lyft_drivers
 WHERE yearly_salary <= 30000
     OR yearly_salary >= 70000;
+
+
+-- Prompt 41: Find the advertising channel(s) where Uber spent more than 100k USD in 2019.
+SELECT advertising_channel
+FROM uber_advertising
+WHERE year = 2019
+    AND money_spent > 100000;
