3f5920afb0db68bcf57ba805bf49391c81fcb8d5
selenanguyen980 <selenanguyen980@gmail.com>
2026-04-24 11:56:10 -0700

    Update query_submissions.sql

--- query_submissions.sql
+++ query_submissions.sql
@@ -66,16 +66,18 @@ WHERE rn = 1
 ORDER BY year ASC, noc ASC;
 
 
--- Prompt 4: As a data scientist at Amazon Prime Video, you are tasked with enhancing the in-flight entertainment experience for Amazon’s airline partners.
--- For flight 101, find movies whose runtime is less than or equal to the flight's duration.
+-- Prompt 4: Count the number of unique users per day who logged in from either a mobile device or web.
+-- Output the date and the corresponding number of users.
+WITH total_logs AS (
+    SELECT user_id, log_date FROM mobile_logs
+    UNION ALL
+    SELECT user_id, log_date FROM web_logs
+)
 SELECT
-    f.flight_id,
-    m.movie_id,
-    m.duration
-FROM flight_schedule AS f
-INNER JOIN entertainment_catalog AS m
-    ON f.flight_duration >= m.duration
-WHERE f.flight_id = 101;
+    log_date,
+    COUNT(DISTINCT user_id) AS n_users
+FROM total_logs
+GROUP BY log_date;
 
 
 -- Prompt 5: Rank the top five customers by total purchase value. If multiple customers have the same total purchase value, treat them as ties and include all tied customers in the result.
@@ -107,7 +109,7 @@ SELECT
 FROM airbnb_guests;
 
 
--- Prompt 7: Return a list of users with status free who didn’t make any calls in Apr 2020.
+-- Prompt 7: Return a list of users with status free who didn't make any calls in Apr 2020.
 SELECT
     u.user_id
 FROM rc_users AS u
@@ -131,21 +133,7 @@ LEFT JOIN sales_transactions AS s
 WHERE s.product_id IS NULL;
 
 
--- Prompt 9: Count the number of unique users per day who logged in from either a mobile device or web.
--- Output the date and the corresponding number of users.
-WITH total_logs AS (
-    SELECT user_id, log_date FROM mobile_logs
-    UNION ALL
-    SELECT user_id, log_date FROM web_logs
-)
-SELECT
-    log_date,
-    COUNT(DISTINCT user_id) AS n_users
-FROM total_logs
-GROUP BY log_date;
-
-
--- Prompt 10: Return the total number of comments received for each user in the 30-day period up to and including 2020-02-10.
+-- Prompt 9: Return the total number of comments received for each user in the 30-day period up to and including 2020-02-10.
 -- Don't output users who haven't received any comment in the defined time period.
 SELECT
     user_id,
@@ -156,7 +144,7 @@ WHERE created_at >= '2020-02-10' - INTERVAL 30 DAY
 GROUP BY user_id;
 
 
--- Prompt 11: Calculate the average score for each project, but only include projects where more than one team member has provided a score.
+-- Prompt 10: Calculate the average score for each project, but only include projects where more than one team member has provided a score.
 -- Your output should include the project ID and the calculated average score for each qualifying project.
 SELECT
     project_id,
@@ -166,7 +154,7 @@ GROUP BY project_id
 HAVING COUNT(DISTINCT team_member_id) > 1;
 
 
--- Prompt 12: Count the unique activity types for each user, ensuring users with no activities are also included.
+-- Prompt 11: Count the unique activity types for each user, ensuring users with no activities are also included.
 -- The output should show each user's ID and their activity type count, with zero for users who have no activities.
 SELECT
     u.user_id,
@@ -175,6 +163,18 @@ FROM user_profiles AS u
 LEFT JOIN activity_log AS a
     ON u.user_id = a.user_id
 GROUP BY u.user_id;
+
+
+-- Prompt 12: As a data scientist at Amazon Prime Video, you are tasked with enhancing the in-flight entertainment experience for Amazon's airline partners.
+-- For flight 101, find movies whose runtime is less than or equal to the flight's duration.
+SELECT
+    f.flight_id,
+    m.movie_id,
+    m.duration
+FROM flight_schedule AS f
+INNER JOIN entertainment_catalog AS m
+    ON f.flight_duration >= m.duration
+WHERE f.flight_id = 101;
 
 
 -- Prompt 13: You're tasked with analyzing a Spotify-like dataset that captures user listening habits.
@@ -187,7 +187,7 @@ FROM listening_habits
 GROUP BY user_id;
 
 
--- Prompt 14: Find the average number of bathrooms and bedrooms for each city’s property types.
+-- Prompt 14: Find the average number of bathrooms and bedrooms for each city's property types.
 -- Output the result along with the city name and the property type.
 SELECT
     city,
@@ -198,15 +198,30 @@ FROM airbnb_search_details
 GROUP BY city, property_type;
 
 
--- Prompt 15: Find all the songs that were top-ranked (at first position) at least once since the year 2005.
-SELECT DISTINCT
-    song_name
-FROM billboard_top_100_year_end
-WHERE year_rank = 1
-  AND year >= 2005;
+-- Prompt 15: Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
+-- Based on the above, find the average popularity of the Hack per office location.
+-- Output the location along with the average popularity.
+SELECT e.location,
+    AVG(h.popularity) AS avg_popularity
+FROM facebook_employees AS e
+INNER JOIN facebook_hack_survey AS h
+ON e.id = h.employee_id
+GROUP BY e.location;
 
 
--- Prompt 16: Find the details of each customer regardless of whether the customer made an order.
+-- Prompt 16: Determine whether hosts or guests leave higher review scores on average.
+-- Return the group (host or guest) with the higher average score and the corresponding average rounded to 2 decimal places.
+SELECT from_type,
+   AVG(review_score) AS high_avg_score
+FROM airbnb_reviews
+WHERE from_type = 'guest'
+    OR from_type = 'host'
+GROUP BY from_type
+ORDER BY high_avg_score DESC
+LIMIT 1;
+
+
+-- Prompt 17: Find the details of each customer regardless of whether the customer made an order.
 -- Output the customer's first name, last name, and the city along with the order details.
 SELECT
     c.first_name,
@@ -219,84 +234,7 @@ LEFT JOIN orders AS o
 ORDER BY c.first_name, o.order_details ASC;
 
 
--- Prompt 17: Find doctors with the last name of 'Johnson' in the employee list.
--- The output should contain both their first and last names.
-SELECT
-    first_name,
-    last_name
-FROM employee_list
-WHERE lower(last_name) = 'johnson'
-  AND lower(profession) = 'doctor';
-
-
--- Prompt 18: Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
--- Based on the above, find the average popularity of the Hack per office location.
--- Output the location along with the average popularity.
-SELECT e.location,
-    AVG(h.popularity) AS avg_popularity
-FROM facebook_employees AS e
-INNER JOIN facebook_hack_survey AS h
-ON e.id = h.employee_id
-GROUP BY e.location;
-
-
--- Prompt 19: Determine whether hosts or guests leave higher review scores on average.
--- Return the group (host or guest) with the higher average score and the corresponding average rounded to 2 decimal places.
-SELECT from_type,
-   AVG(review_score) AS high_avg_score
-FROM airbnb_reviews
-WHERE from_type = 'guest'
-    OR from_type = 'host'
-GROUP BY from_type
-ORDER BY high_avg_score DESC
-LIMIT 1;
-
-
--- Prompt 20: Find the number of relationships that user  with id == 1 is not part of.
-SELECT COUNT(*) AS relationships_without_user1
-FROM facebook_friends
-WHERE user1 <> 1
-    AND user2 <> 1;
-
-
--- Prompt 21: Find the total number of searches for houses in Westlake neighborhood with a TV among the amenities.
-SELECT COUNT(id) AS n_searches
-FROM airbnb_search_details
-WHERE property_type = 'House'
-    AND neighbourhood = 'Westlake'
-    AND amenities LIKE '%TV%';
-
-
--- Prompt 22: Find the gender that has made the most number of doctor appointments.
--- Output the gender along with the corresponding number of appointments.
-SELECT
-    gender,
-    COUNT(appointmentid) AS n_appointments
-FROM medical_appointments
-GROUP BY gender
-HAVING gender = 'F';
-
-
--- Prompt 23: Find the total number of records that belong to each variety in the dataset.
--- Output the variety along with the corresponding number of records. Order records by the variety in ascending order.
-SELECT
-    variety,
-    COUNT(*)
-FROM iris
-GROUP BY variety;
-
-
--- Prompt 24: Find the total number of housing units completed for each year.
--- Output the year along with the total number of housings. Order the result by year in ascending order.
-SELECT DISTINCT
-    year,
-    SUM(south + west + midwest + northeast) AS total_n_housings
-FROM housing_units_completed_us
-GROUP BY year
-ORDER BY year ASC;
-
-
--- Prompt 25: Find how many reviews exist for each review score given to 'Hotel Arena'.
+-- Prompt 18: Find how many reviews exist for each review score given to 'Hotel Arena'.
 -- Output the hotel name ('Hotel Arena'), each review score, and the number of reviews for that score.
 SELECT
     hotel_name,
@@ -308,23 +246,17 @@ GROUP BY hotel_name,
     reviewer_score;
 
 
--- Prompt 26: Find the total AdWords earnings for each business type.
--- Output the business types along with the total earnings.
-SELECT
-    business_type,
-    SUM(adwords_earnings) AS total_earnings
-FROM google_adwords_earnings
-GROUP BY business_type;
+-- Prompt 19: Find the number of reviews received by Lo-Lo's Chicken & Waffles for each star. 
+-- Output the number of stars along with the corresponding number of reviews. Sort records by stars in ascending order.
+SELECT stars,
+    COUNT(review_id) AS n_reviews
+FROM yelp_reviews
+WHERE business_name = 'Lo-lo''s Chicken & Waffles'
+GROUP BY stars
+ORDER BY stars ASC;
 
 
--- Prompt 27: Find the number of Yelp businesses that sell pizza.
-SELECT
-    COUNT(business_id) AS n_of_businesses
-FROM yelp_business
-WHERE categories LIKE '%pizza%';
-
-
--- Prompt 28: Find employees who started in June and have even-numbered employee IDs.
+-- Prompt 20: Find employees who started in June and have even-numbered employee IDs.
 SELECT *
 FROM worker
 WHERE joining_date >= '2014-06-01'
@@ -332,12 +264,82 @@ WHERE joining_date >= '2014-06-01'
   AND worker_id % 2 = 0;
 
 
--- Prompt 29: Find employees who started in February and have odd-numbered employee IDs.
+-- Prompt 21: Find employees who started in February and have odd-numbered employee IDs.
 SELECT *
 FROM worker
 WHERE joining_date >= '2014-02-01'
   AND joining_date < '2014-03-01'
   AND worker_id % 2 = 1;
+
+
+-- Prompt 22: Find wine varieties tasted by 'Roger Voss' and with a value in the 'region_1' column of the dataset. Output unique variety names only.
+SELECT DISTINCT variety
+FROM winemag_p2
+WHERE taster_name = 'Roger Voss'
+    AND region_1 IS NOT NULL;
+
+
+-- Prompt 23: Find the total costs and total customers acquired in each year.
+-- Output the year along with corresponding total money spent and total acquired customers.
+SELECT year,
+    SUM(customers_acquired) AS total_customers,
+    SUM(money_spent) AS total_costs
+FROM uber_advertising
+GROUP BY year;
+
+
+-- Prompt 24: Find the average cost of each request status. Request status can be either 'success' or 'fail'.
+-- Output the request status along with the average cost.
+SELECT request_status,
+    AVG(monetary_cost) AS avg_cost
+FROM uber_ride_requests
+WHERE request_status = 'success'
+    OR request_status = 'fail'
+GROUP BY request_status;
+
+
+-- Prompt 25: Find the hour with the highest gasoline cost. Assume there's only 1 hour with the highest gas cost.
+SELECT hour
+FROM lyft_rides
+GROUP BY hour
+ORDER BY MAX(gasoline_cost) DESC
+LIMIT 1;
+
+
+-- Prompt 26: Find the gender that has made the most number of doctor appointments.
+-- Output the gender along with the corresponding number of appointments.
+SELECT
+    gender,
+    COUNT(appointmentid) AS n_appointments
+FROM medical_appointments
+GROUP BY gender
+HAVING gender = 'F';
+
+
+-- Prompt 27: Find the number of US-based wineries that have expensive wines. A wine is considered to be expensive if its price is $200 or more.
+SELECT COUNT(DISTINCT winery) AS n_wineries
+FROM winemag_p1
+WHERE country = 'US'
+  AND price >= 200;
+
+
+-- Prompt 28: Find the total number of housing units completed for each year.
+-- Output the year along with the total number of housings. Order the result by year in ascending order.
+SELECT DISTINCT
+    year,
+    SUM(south + west + midwest + northeast) AS total_n_housings
+FROM housing_units_completed_us
+GROUP BY year
+ORDER BY year ASC;
+
+
+-- Prompt 29: Find the total number of records that belong to each variety in the dataset.
+-- Output the variety along with the corresponding number of records. Order records by the variety in ascending order.
+SELECT
+    variety,
+    COUNT(*)
+FROM iris
+GROUP BY variety;
 
 
 -- Prompt 30: Find the number of crime occurrences for each day of the week.
@@ -349,31 +351,16 @@ FROM sf_crime_incidents_2014_01
 GROUP BY day_of_week;
 
 
--- Prompt 31: What is the total sales revenue of Samantha and Lisa?
+-- Prompt 31: Find the total AdWords earnings for each business type.
+-- Output the business types along with the total earnings.
 SELECT
-    SUM(sales_revenue) AS total_revenue
-FROM sales_performance
-WHERE salesperson = 'Samantha'
-   OR salesperson = 'Lisa';
-
-*
--- Prompt 32: Find the number of reviews received by Lo-Lo's Chicken & Waffles for each star. 
--- Output the number of stars along with the corresponding number of reviews. Sort records by stars in ascending order.
-SELECT stars,
-    COUNT(review_id) AS n_reviews
-FROM yelp_reviews
-WHERE business_name = 'Lo-lo''s Chicken & Waffles'
-GROUP BY stars
-ORDER BY stars ASC;
+    business_type,
+    SUM(adwords_earnings) AS total_earnings
+FROM google_adwords_earnings
+GROUP BY business_type;
 
 
--- Prompt 33: Find the number of 5-star reviews earned by Lo-Lo's Chicken & Waffles.
-SELECT COUNT(stars) AS n_of_5stars
-FROM yelp_reviews
-WHERE business_name = 'Lo-Lo''s Chicken & Waffles'
-    AND stars = '5';
-
--- Prompt 34: Find the number of entries per star.
+-- Prompt 32: Find the number of entries per star.
 -- Output each number of stars along with the corresponding number of entries. Order records by stars in ascending order.
 SELECT stars,
     COUNT(review_id) AS n_entries
@@ -382,55 +369,54 @@ GROUP BY stars
 ORDER BY stars ASC;
 
 
--- Prompt 34: Find the number of US-based wineries that have expensive wines. A wine is considered to be expensive if its price is $200 or more.
-SELECT COUNT(DISTINCT winery) AS n_wineries
-FROM winemag_p1
-WHERE country = 'US'
-  AND price >= 200;
+-- Prompt 33: Find the total number of searches for houses in Westlake neighborhood with a TV among the amenities.
+SELECT COUNT(id) AS n_searches
+FROM airbnb_search_details
+WHERE property_type = 'House'
+    AND neighbourhood = 'Westlake'
+    AND amenities LIKE '%TV%';
 
 
--- Prompt 35: Find wine varieties tasted by 'Roger Voss' and with a value in the 'region_1' column of the dataset. Output unique variety names only.
-SELECT DISTINCT variety
-FROM winemag_p2
-WHERE taster_name = 'Roger Voss'
-    AND region_1 IS NOT NULL;
+-- Prompt 34: Find the number of relationships that user  with id == 1 is not part of.
+SELECT COUNT(*) AS relationships_without_user1
+FROM facebook_friends
+WHERE user1 <> 1
+    AND user2 <> 1;
 
 
--- Prompt 36: Find the total costs and total customers acquired in each year.
--- Output the year along with corresponding total money spent and total acquired customers.
-SELECT year,
-    SUM(customers_acquired) AS total_customers,
-    SUM(money_spent) AS total_costs
-FROM uber_advertising
-GROUP BY year;
+-- Prompt 35: Find all the songs that were top-ranked (at first position) at least once since the year 2005.
+SELECT DISTINCT
+    song_name
+FROM billboard_top_100_year_end
+WHERE year_rank = 1
+  AND year >= 2005;
 
 
--- Prompt 37: Find the average cost of each request status. Request status can be either 'success' or 'fail'.
--- Output the request status along with the average cost.
-SELECT request_status,
-    AVG(monetary_cost) AS avg_cost
-FROM uber_ride_requests
-WHERE request_status = 'success'
-    OR request_status = 'fail'
-GROUP BY request_status;
+-- Prompt 36: Find the number of Yelp businesses that sell pizza.
+SELECT
+    COUNT(business_id) AS n_of_businesses
+FROM yelp_business
+WHERE categories LIKE '%pizza%';
 
 
--- Prompt 38: Find the hour with the highest gasoline cost. Assume there's only 1 hour with the highest gas cost.
-SELECT hour
-FROM lyft_rides
-GROUP BY hour
-ORDER BY MAX(gasoline_cost) DESC
-LIMIT 1;
+-- Prompt 37: Find doctors with the last name of 'Johnson' in the employee list.
+-- The output should contain both their first and last names.
+SELECT
+    first_name,
+    last_name
+FROM employee_list
+WHERE lower(last_name) = 'johnson'
+  AND lower(profession) = 'doctor';
 
 
--- Prompt 39: Find all Lyft rides which happened on rainy days before noon.
+-- Prompt 38: Find all Lyft rides which happened on rainy days before noon.
 SELECT *
 FROM lyft_rides
 WHERE weather = 'rainy'
     AND hour < 12;
 
 
--- Prompt 40: Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
+-- Prompt 39: Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
 Output all details related to retrieved records.
 SELECT *
 FROM lyft_drivers
@@ -438,14 +424,14 @@ WHERE yearly_salary <= 30000
     OR yearly_salary >= 70000;
 
 
--- Prompt 41: Find the advertising channel(s) where Uber spent more than 100k USD in 2019.
+-- Prompt 40: Find the advertising channel(s) where Uber spent more than 100k USD in 2019.
 SELECT advertising_channel
 FROM uber_advertising
 WHERE year = 2019
     AND money_spent > 100000;
 
 
--- Prompt 42: Find songs that are ranked between 8-10.
+-- Prompt 41: Find songs that are ranked between 8-10.
 -- Output the track name along with the corresponding position, ordered ascendingly.
 SELECT trackname,
     position
@@ -455,11 +441,18 @@ WHERE position BETWEEN 8
 ORDER BY position ASC;
 
 
--- Prompt 43: Find the year that Uber acquired more than 2000 customers through advertising using celebrities.
+-- Prompt 42: Find the year that Uber acquired more than 2000 customers through advertising using celebrities.
 SELECT year
 FROM uber_advertising
 WHERE advertising_channel = 'celebrities'
     AND customers_acquired > 2000;
+
+
+-- Prompt 43: Find the number of 5-star reviews earned by Lo-Lo's Chicken & Waffles.
+SELECT COUNT(stars) AS n_of_5stars
+FROM yelp_reviews
+WHERE business_name = 'Lo-Lo''s Chicken & Waffles'
+    AND stars = '5';
 
 
 -- Prompt 44: Find the total number of streams for the top 100 ranked songs.
@@ -467,3 +460,11 @@ SELECT SUM(streams) AS total_n_streams
 FROM spotify_worldwide_daily_song_ranking
 WHERE position BETWEEN 1
     AND 100;
+
+
+-- Prompt 45: What is the total sales revenue of Samantha and Lisa?
+SELECT
+    SUM(sales_revenue) AS total_revenue
+FROM sales_performance
+WHERE salesperson = 'Samantha'
+   OR salesperson = 'Lisa';
